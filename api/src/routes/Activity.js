881887315b28e9704189706cbcb358afce6b8cb5
Mateo Dominguez <matedom22@gmail.com>
2022-03-16 12:59:48 -0300

    Routes from Backend

--- api/src/routes/Activity.js
+++ api/src/routes/Activity.js
@@ -1,14 +1,26 @@
 const { Router } = require('express');
-const {Activity} = require('../db')
-
+const {Activity, Country} = require('../db')
+const Sequelize = require('sequelize')
 
 const router = Router();
 router.post('/', async (req, res)=>{
-    const {name, duration, difficulty, season} = req.body
-    const NewActivity = await Activity.create({
+    const {name, duration, difficulty, season, country} = req.body
+    const createActivity = await Activity.create({
         name: name, duration: duration, difficulty: difficulty, season: season
     })
-    res.send(NewActivity)
+
+    const countryActivity = await Country.findOne({
+        where: {
+            name: {[Sequelize.Op.iLike] : `${country}`}
+        }
+    })
+    if(countryActivity){
+       await createActivity.addCountry(countryActivity)
+       res.send("Actividad creada exitosamente")
+    } else{
+        res.send("Lo sentimos, no pudimos crear esa actividad")
+    }
+ 
 
 })
 
--- api/src/routes/Country.js
+++ api/src/routes/Country.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
-const {Country, Activity} = require('../db')
+const {Country, Activity } = require('../db')
+const Sequelize = require('sequelize')
 const axios = require('axios')
 
+const router = Router();
 
 const CountriesAPI = async ()=>{
     try{
-
         const allCountries = await axios.get("https://restcountries.com/v3/all")
         const countries = await allCountries.data.map((c)=>{
             return {
@@ -13,7 +14,7 @@ const CountriesAPI = async ()=>{
                 id: c.cca3,
                 flag: c.flags[0],
                 continent: c.region,
-                capital: !c.capital? c.capital = "": c.capital[0],
+                capital: !c.capital? c.capital = "No tiene capital": c.capital[0],
                 subregion: c.subregion,
                 area: c.area,
                 population: c.population
@@ -30,19 +31,68 @@ const CountriesAPI = async ()=>{
 }
 
 
-const router = Router();
-router.get('/', (req, res, next)=>{
-    res.send('get countries')
+
+router.get('/', async (req, res, next)=>{
+    const queryname = req.query.name
+    try{
+
+        const find = await Country.findAll({
+            include: {
+                model: Activity}
+        })
+        const countryDb = await CountriesAPI();
+        if(!find.length){
+            for(let i= 0; i< countryDb.length; i++){
+                await Country.create(countryDb[i])
+            }
+          
+}} 
+   catch(err){
+      return err} 
+    
+    
+      try{
+
+        if(queryname){
+          const findByQuery = await Country.findAll({
+              where: {
+                  name:  {[Sequelize.Op.iLike]: `%${queryname}%`}
+              }
+          })
+          if(findByQuery.length){
+               res.status(200).send(findByQuery)
+          } else {
+              res.status(404).send("No existe dicho pais")
+          }
+      }
+     
+      else{
+          const find = await Country.findAll({
+              include: {
+                  model: Activity}
+          })
+          res.status(200).send(find)
+      }
+    } catch(err){
+        return err
+    }
 
 })
-router.get('/:idPais', (req, res, next)=>{
-    res.send('get countries id')
-
+router.get('/:idPais', async (req, res, next)=>{
+    const paisId = req.params.idPais
+    const findById = await Country.findOne({
+        where : {id : paisId},
+        include : {model: Activity}
+    })
+   
+    if(findById){
+        res.status(200).send(findById)
+    } else {
+        res.status(404).send('Lo sentimos no encontramos ese pais')
+    }
+    
 })
-router.get('/', (req, res, next)=>{
-    res.send('get countries')
 
-})
 
 
 module.exports = router;
